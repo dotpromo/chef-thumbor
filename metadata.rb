96aa70d9435490d32e682a54ca9271eea8539eef
Alexander Simonov <alex@simonov.me>
2015-09-24 10:44:32 +0300

    Fix support version

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.2'
 
 supports 'ubuntu', '>= 12.04'
-supports 'centos', '>= 6.5', '< 7.0'
+supports 'centos', '~> 6.5'
 
 recipe 'thumbor::default', 'Installs and configures thumbor'
 
